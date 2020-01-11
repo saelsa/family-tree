01e2eb5e208d4f7e8ae7cb9afeab96eee5c711ce
Sarah Saak <sarah.saak@meaningfuldata.eu>
2020-01-11 12:55:58 +0100

    feat: Specify basic model and API for persons and events

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -9,40 +10,170 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-from models import Person
-
-@app.route("/")
-def hello():
-    return "Hello World!"
+from models import Person, Event
 
 
-@app.route("/add")
+'''
+PERSONS
+'''
+
+@app.route("/persons", methods=['GET'])
+def get_persons(): 
+
+    try:
+        persons = Person.query.all()
+
+        return jsonify({
+            'success': True,
+            'persons': [person.serialize() for person in persons]
+        })
+    except: 
+        abort(404)
+
+
+
+@app.route("/persons", methods=['POST'])
+# @requires_auth('post:drinks')
 def add_person():
-    firstname=request.args.get('firstname')
-    lastname=request.args.get('lastname')
-    try: 
-        person=Person(firstname=firstname, lastname=lastname)
-        db.session.add(person)
-        db.session.commit()
-        return "Person added. book id={}".format(person.id)
-    except Exception as e: 
-        return(str(e))
 
-@app.route("/getall")
-def get_all():
-    try:
-        persons=Person.query.all()
-        return jsonify([e.serialize() for e in persons])
-    except Exception as e: 
-        return(str(e))
+    body = request.get_json()
 
-@app.route("/get/<id_>")
-def get_by_id(id_):
+    if not ('firstname' in body and 'lastname' in body and 'birthdate' in  body):
+        abort(404)
+
+    firstname = body.get('firstname')
+    lastname = body.get('lastname')
+    birthdate = body.get('birthdate')
+
     try:
-        person=Person.query.filter_by(id=id).first()
-        return jsonify(person.serialize())
-    except Exception as e: 
-        return(str(e))
+        person = Person(firstname=firstname, lastname=lastname, birthdate=birthdate)
+        person.insert()
+
+        return jsonify({
+            'success': True
+        })
+
+    except:
+        abort(422)
+
+@app.route("/persons/<id>", methods=['PATCH'])
+# @requires_auth('post:drinks')
+def update_person(id):
+
+    person = Person.query.get(id)
+
+    if person: 
+        try: 
+            body = request.get_json()
+
+            firstname = body.get('firstname')
+            lastname = body.get('lastname')
+            birthdate = body.get('birthdate')
+
+            print(body)
+
+            if firstname:
+                person.firstname = firstname
+            if lastname: 
+                person.lastname = lastname
+            if birthdate: 
+                person.birthdate = birthdate
+            
+            person.update()
+
+            return jsonify({
+                'success': True
+            })
+        except: 
+            abort(422)
+    else:
+        abort(404)
+
+@app.route("/persons/<id>", methods=['DELETE'])
+# @requires_auth('delete:drinks')
+def delete_person(id):
+
+    person = Person.query.get(id)
+
+    if person:
+        try:
+            person.delete()
+            return jsonify({
+                'success': True,
+                'delete': id
+            })
+        except:
+            abort(422)
+    else:
+        abort(404)
+
+
+'''
+EVENTS
+'''
+
+@app.route("/events", methods=['GET'])
+def get_events(): 
+
+    try:
+        events = Event.query.all()
+
+        return jsonify({
+            'success': True,
+            'events': [event.serialize() for event in events]
+        })
+    except: 
+        abort(404)
+
+
+@app.route("/events", methods=['POST'])
+# @requires_auth('post:drinks')
+def add_event():
+
+    body = request.get_json()
+
+    print(body)
+    if not ('event_type' in body and 'date' in body):
+        abort(404)
+
+    event_type = body.get('event_type')
+    date = body.get('date')
+
+    try:
+        event = Event(event_type=event_type, date=date)
+        event.insert()
+
+        return jsonify({
+            'success': True
+        })
+
+    except:
+        abort(422)
+
+
+
+
+# Error Handling
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": str(error)
+    }), 422
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": str(error)
+    }), 404
+
+
+
 
 
 
--- models.py
+++ models.py
@@ -5,19 +5,68 @@ class Person(db.Model):
     __tablename__ = 'persons'
 
     id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String())
-    lastname = db.Column(db.String())
+    firstname = db.Column(db.String(), nullable=False)
+    lastname = db.Column(db.String(), nullable=False)
+    birthdate = db.Column(db.String())
 
-    def __init__(self, firstname, lastname):
+    def __init__(self, firstname, lastname, birthdate):
         self.firstname = firstname
         self.lastname = lastname
+        self.birthdate = birthdate
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
+
+    def insert(self): 
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
 
     def serialize(self):
         return {
             'id': self.id,
             'firstname': self.firstname,
-            'lastname': self.lastname
+            'lastname': self.lastname,
+            'birthdate': self.birthdate,
+        }
+
+
+class Event(db.Model):
+    __tablename__ = 'events'
+
+    id = db.Column(db.Integer, primary_key=True)
+    event_type = db.Column(db.String(), nullable=False)
+    date = db.Column(db.String())
+
+
+
+    def __init__(self, event_type, date):
+        self.event_type = event_type
+        self.date = date
+    
+    def __repr__(self): 
+        return '<id {}>'.format(self.id)
+
+    def insert(self): 
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+    
+    def serialize(self): 
+        return {
+            'id': self.id,
+            'event_type': self.event_type,
+            'date': self.date
         }
