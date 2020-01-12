8b40fd1453e5d4621f0889e92bbed4bb7d67f416
Sarah Saak <sarah.saak@meaningfuldata.eu>
2020-01-12 19:39:18 +0100

    feat: Change callback uri

--- constants.py
+++ constants.py
@@ -2,6 +2,6 @@
 """
 AUTH0_CLIENT_ID = 'R9jRqp35T1BqdNfhfyNdaL21mf1kCD59'
 AUTH0_CLIENT_SECRET = 'NQUlkybx8xc1RzhRrfMxljItYeq2P2HpktVNDpIZyk7UEO3DmrUS_KjloRtBRSOG'
-AUTH0_CALLBACK_URL = 'http://localhost:5000/callback'
+AUTH0_CALLBACK_URL = 'https://fsnd-family-tree.herokuapp.com/callback'
 AUTH0_DOMAIN = 'saelsa.eu.auth0.com'
 AUTH0_AUDIENCE = 'https://family-tree'
